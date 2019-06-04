7ee5059c19cfc20727a2a74a24a0f8e33fcf6c09
Paul Davis <pauldavis@Pauls-MacBook-Pro.local>
2019-06-04 16:17:32 -0400

    firebase setup - working on pulling data

--- assets/javascript/getStandard-coordinator.js
+++ assets/javascript/getStandard-coordinator.js
@@ -10,10 +10,43 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const database = firebase.database();
 
-firebase.auth().onAuthStateChanged(function(firebaseUser) {
-  if (firebaseUser) {
-    console.log('welcome')
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    let currentUser = user.email
+    db.collection('users').doc(currentUser).get().then(function(standardSearch) {
+      let yourStandard = standardSearch.data().standard
+      db.collection('standards').doc(yourStandard).get().then(function(standardFound) {
+        // console.log(standardFound.data())
+        $.each(standardFound.data(), function(key, value){
+          let buildingStandardTitle = key
+          let buildingStandard = value
+          let where = Object.keys(buildingStandard) // where the location of the work is
+          let what = Object.values(buildingStandard) // what the work is to be done
+
+          $('<div>')
+            .addClass('card')
+            .appendTo($('#standard'))
+
+          for (let i = 0; i < where.length; i++) {
+            $('<div>')
+              .addClass('card-header')
+              .text(where[i])
+              .appendTo($('.card'))
+            $('<div>')
+              .addClass('card-body')
+              .appendTo($('.card'))      
+          }
+
+          // for(let j = 0; j < what.length; j++) {
+          //   console.log(Object.keys(what[i]))
+          // }
+          
+        })
+      })
+    })
   } else {
     console.log('not logged in')
   }
